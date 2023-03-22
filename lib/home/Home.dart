e9896059a8205f38c25f4e8d390079ec72c60654
priyanshu singh <priyanshusingh@priyanshus-MacBook-Air.local>
2023-03-22 14:49:45 +0530

    all setup for firebase login page

---
+++ lib/home/Home.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+class Home_page extends StatefulWidget {
+  const Home_page({super.key});
+
+  @override
+  State<Home_page> createState() => _Home_pageState();
+}
+
+class _Home_pageState extends State<Home_page> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.amber,
+      appBar: AppBar(
+        backgroundColor: Colors.amber,
+        title: Text(
+          'Brew',
+          style: TextStyle(
+            color: Colors.black,
+          ),
+        ),
+        centerTitle: true,
+      ),
+    );
+  }
+}
